413d12196d210ec626148cd44c140f3aefa5a093
Benjamin Etheredge <benjamin.etheredge@gmail.com>
2019-11-21 23:33:05 -0600

    Stopped GPU memory growth. Fixed Double-Q Q-prime function. Renamed some variables.

--- learner.py
+++ learner.py
@@ -1,8 +1,12 @@
 import logging
 
 import numpy as np
-from tensorflow._api.v1 import keras
+import tensorflow as tf
+from tensorflow import keras
 
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
 
 class DeepQ:
     def __init__(self, inputDiminsion, outputDiminsion,
@@ -65,40 +69,52 @@ class DeepQ:
         # TODO test without target
         statePrimes = sample.nextStates
         # Get the action values for state primes NOTE TARGET MODEL
-        allPrimeActionValues = self.targetModel.predict(statePrimes)
-        return allPrimeActionValues
+        target_prime_action_values = self.targetModel.predict(statePrimes)
+        return target_prime_action_values
 
     # TODO maybe make method passed in?
-    def qPrime(self, expectedValues, _):
+    def qPrime(self, prime_action_values, action_values):
+        """
+        Return the Q prime for Deep Q
+        :param prime_action_values: expected values of actions for state prime
+        :param action_values:
+        :return:
+        """
         logging.debug('DeepQ - qPrime')
-        return np.max(expectedValues)
+        return np.max(prime_action_values)
 
     def update(self, sample):
         # TODO refactor
-        states = sample.states
-        allActionValues = self.model.predict(states)
-        allPrimeActionValues = self.targetModel.predict(sample.nextStates)
+        current_all_action_values = self.model.predict(sample.states)
+        current_all_prime_action_values = self.model.predict(sample.nextStates)
+        target_all_prime_action_values = self.targetModel.predict(sample.nextStates)
 
         idx = 0
-        for primeActionValue, action, reward, isDone in zip(allPrimeActionValues, sample.actions, sample.rewards, sample.isDones):
-            qprime = 0
+        for primeActionValue, action, reward, isDone in zip(target_all_prime_action_values, sample.actions, sample.rewards, sample.isDones):
+            q_prime = 0
             if not isDone:
                 # TODO refactor
-                qprime = self.qPrime(allPrimeActionValues[idx], allActionValues[idx])
+                q_prime = self.qPrime(target_all_prime_action_values[idx], current_all_prime_action_values[idx])
+                #q_prime = self.qPrime(target_all_prime_action_values[idx], current_all_action_values[idx])
 
-            actualValue = (self.gamma * qprime) + reward
-            allActionValues[idx][action] = actualValue
+            actual_value = (self.gamma * q_prime) + reward
+            current_all_action_values[idx][action] = actual_value
             idx += 1
 
         # TODO refactor
 
-        self.model.fit(x=states, y=allActionValues, batch_size=self.batchSize, epochs=1, verbose=0)
+        self.model.fit(x=sample.states, y=current_all_action_values, batch_size=self.batchSize, epochs=1, verbose=0)
 
 
 class DoubleDeepQ(DeepQ):
+    """
+    Reduce the overestimations by breaking up action seleciton and action evaluation
+    """
 
-    def qPrime(self, primeActionValues, actionValues):
-        return primeActionValues[np.argmax(actionValues)]
+    def qPrime(self, prime_action_values, action_values):
+        max_action = np.argmax(prime_action_values)
+
+        return prime_action_values[np.argmax(action_values)]
 
 # NOTE Extends DoubleDeepQ and NOT vanilla DeepQ
 class DuelDeepQ(DoubleDeepQ):
