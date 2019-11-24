8d5b6b47ee63c454fc6636372108eb70b82b3b24
Benjamin Etheredge <benjamin.etheredge@gmail.com>
2019-11-24 14:01:52 -0600

    Adding dependency injection for learner.

--- agent.py
+++ agent.py
@@ -7,6 +7,7 @@ from buffer import *
 from scores import *
 
 from timeit import default_timer as timer
+from learner import *
 
 
 class Agent:
@@ -44,27 +45,27 @@ class Agent:
         #self.randomChoiceDecayRate = float(np.power(self.max_episode_steps*300, (1./0.05)))
         self.randomChoiceMinRate = random_choice_decay_min
 
-    def isDoneLearning(self):
+    def is_done_learning(self):
         logging.debug('isDoneLearning')
-        return self.scores.averageReward() >= self.rewardThreshold
+        return self.scores.averageReward() >= self.reward_stopping_threshold
 
     def shouldSelectRandomAction(self):
         logging.debug('shouldSelectRandomAction')
-        return random.uniform(0, 1) < self.randomChoicePercentage
+        return random.uniform(0, 1) < self.random_action_rate
 
     def shouldUpdateLearner(self):
         logging.debug('shouldUpdateLearner')
-        return self.replayBuffer.isReady()
+        return self.replay_buffer.isReady()
 
     def shouldUpdateLearnerTargetModel(self, iteration):
-        return iteration % self.targetNetworkThreshold == 0
+        return iteration % self.target_network_updating_interval == 0
 
     # TODO why should this be a property?
     def shouldDecayRandomChoiceRate(self):
         logging.debug('shouldDecayRandomChoiceRate')
-        return self.replayBuffer.isReady()
+        return self.replay_buffer.isReady()
 
-    def getNextAction(self, state):
+    def getNextAction(self, state, random_choice_rate=None):
         logging.debug('getNextAction')
         if self.shouldSelectRandomAction():
             logging.debug('selecting randomly')
@@ -75,99 +76,133 @@ class Agent:
 
     def decayRandomChoicePercentage(self):
         logging.debug('decayRandomChoice')
-        self.randomChoicePercentage = max(self.randomChoiceMinRate,
-                                          (self.randomChoiceDecayRate * self.randomChoicePercentage))
+        self.random_action_rate = max(self.randomChoiceMinRate,
+                                      (self.randomChoiceDecayRate * self.random_action_rate))
         # self.randomChoicePercentage = minRate + (maxRate - minRate) * np.exp(-decayRate * iteration)
 
     def updateLearner(self):
         logging.debug('updateLearner')
-        sample = self.replayBuffer.sample(self.sampleSize)
+        sample = self.replay_buffer.sample(self.sample_size)
         # npSample = convertSampleToNumpyForm(sample)
         # self.learner.update(npSample)
         self.learner.update(sample)
 
     # TODO implement actual logger
     def shouldLog(self, iteration):
-        return iteration % self.logThreshold == 0
+        return iteration % self.log_triggering_threshold == 0
 
     def log(self):
-        print("info - numberOfExperiences: {0}".format(len(self.replayBuffer)))
-        print("info - randomRate: {0}".format(self.randomChoicePercentage))
+        print(f"info - Game: {self.env.unwrapped.spec.id}")
+        print("info - numberOfExperiences: {0}".format(len(self.replay_buffer)))
+        print("info - randomRate: {0}".format(self.random_action_rate))
+        print(f"info - Reward Target: {self.reward_stopping_threshold}")
         print("info - averageReward: {0}".format(self.scores.averageReward()))
         print("info - randomDecay: {0}".format(self.randomChoiceDecayRate))
-        print("info - sampleSize: {0}".format(self.sampleSize))
-        print("info - targetNetworkThreshold: {0}".format(self.targetNetworkThreshold))
+        print("info - sampleSize: {0}".format(self.sample_size))
+        print("info - targetNetworkThreshold: {0}".format(self.target_network_updating_interval))
+        print(f"info - max_episode_steps: {self.max_episode_steps}")
         # print("info - optimizaer {0}, loss {1}, dequeAmount: {2}".format(optimizer, loss, dequeAmount))
         # TODO paramertize optimizer
         self.learner.log()
-        self.replayBuffer.log()
+        self.replay_buffer.log()
 
-    def play(self):
+    def render_game(self):
+        step = self.scoring_env.reset()
+        is_done = False
+        while not is_done:
+            self.scoring_env.render()
+            action_choice = self.learner.getNextAction(step)
+            _, _, is_done, _ = self.scoring_env.step(action_choice)
+
+    def play(self, step_limit=float("inf"), verbose=0):
         iteration = 0
-        totalSteps = 0
+        total_steps = 0
         start_time = timer()
         iteration_time = start_time
-        while not self.isDoneLearning():
+        while not self.is_done_learning():
             # print("Start Iteration: {}".format(iteration))
             iteration += 1
 
             # Start a new game
-            step = self.env.reset()
-            isDone = False
-            totalReward = 0
-            while not isDone:
-                actionToTake = self.getNextAction(step)
-                totalSteps += 1
-                nextStep, reward, isDone, _ = self.env.step(actionToTake)
-                # TODO add prioirity
-                experience = Experience(step, actionToTake, nextStep, reward, isDone)
-                self.replayBuffer.append(experience)
-                step = nextStep
+            if iteration % 100 == 0:
+                score = self.score_model(100)
+                print(f"\nitermediate score: {score}\n")
+                if score >= self.reward_stopping_threshold:
+                    return total_steps
 
-                if self.replayBuffer.isReady():
+            step = self.env.reset()
+            is_done = False
+            total_reward = 0
+            self.learner.update_target_model()
+            while not is_done:
+                if total_steps > step_limit:
+                    return total_steps
+
+                if verbose > 2:
+                    self.env.render()
+                action_choice = self.getNextAction(step)
+                total_steps += 1
+                next_step, reward, is_done, _ = self.env.step(action_choice)
+                # TODO add prioirity
+                experience = Experience(step, action_choice, next_step, reward, is_done)
+                self.replay_buffer.append(experience)
+                step = next_step
+
+                if self.replay_buffer.isReady():
                     self.updateLearner()
                     self.decayRandomChoicePercentage()
 
-                    if self.shouldUpdateLearnerTargetModel(totalSteps):
-                        self.learner.updateTargetModel()
+                    #if self.shouldUpdateLearnerTargetModel(total_steps):
+                        #self.learner.update_target_model()
 
-                if self.shouldLog(totalSteps):
+                if verbose > 0 and self.shouldLog(total_steps):
                     current_time = timer()
-                    print(f"At Iteration: {iteration}")
-                    print(f"At step: {totalSteps}")
+                    print(f"\nAt Iteration: {iteration}")
+                    print(f"Step Limit: {step_limit}")
+                    print(f"At step: {total_steps}")
                     print(f"Iteration took: {round(current_time - iteration_time, 2)}s")
-                    print(f"Iteration took: {current_time - iteration_time}s")
                     print(f"Total Time: {round(current_time - start_time, 2)}s")
                     iteration_time = current_time
                     self.log()
+                    if verbose > 1:
+                        self.render_game()
 
-                totalReward += reward
+                total_reward += reward
 
-            self.scores.append(totalReward)
+            self.scores.append(total_reward)
 
-        self.plot()
+        #self.plot()
 
-        self.finalScoring()
+        #self.score_model()
+        return total_steps
 
-    def finalScoring(self):
-        self.scores.reset()
-        self.randomChoicePercentage = 0
+    def load_model(self, file_name):
+        #self.learner.load
+        pass
 
-        for _ in range(100):
-            totalReward = 0
+    def save_model(self, file_name):
+        pass
+
+
+    def score_model(self, games=150, verbose=0):
+        scores = Scores(score_count=150)
+
+        for _ in range(games):
+            total_reward = 0
             done = False
-            step = np.array(self.env.reset())
+            step = self.scoring_env.reset()
             while not done:
-                self.env.render()
-                actionToTake = self.getNextAction(step)
-                step, reward, done, _ = self.env.step(actionToTake)
+                if verbose > 0:
+                    self.scoring_env.render()
+                action_choice = self.learner.getNextAction(step)
+                step, reward, done, _ = self.scoring_env.step(action_choice)
 
-                totalReward += reward
+                total_reward += reward
 
-            self.scores.append(totalReward)
+            scores.append(total_reward)
 
-        print("info - Average reward {}".format(self.scores.averageReward()))
+        return scores.averageReward()
 
-    def plot(self):
-        self.scores.plotA()
-        self.scores.plotB()
+    def plot(self, game_name=None, learner_name=None):
+        self.scores.plotA(game_name, learner_name)
+        self.scores.plotB(game_name, learner_name)
