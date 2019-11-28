1ca83a24072fa434a0d5a4fb39ab40f8015147d0
Benjamin Etheredge <benjamin.etheredge@gmail.com>
2019-11-28 11:28:04 -0600

    Format cleanup. Switching to factory methods. Temporary stop after one training cycle to profile.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 import os
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
 import tensorflow as tf
+
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 # Custom Packages
 from agent import *
@@ -16,7 +18,6 @@ from tqdm import tqdm
 
 logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 logging.warning('This will get logged to a file')
-
 
 if __name__ == "__main__":
     import cProfile
@@ -34,79 +35,90 @@ if __name__ == "__main__":
         "MountainCar-v0",
         "Acrobot-v1",
         "LunarLander-v2",
-        ]
+    ]
     games_to_play = [
         400,
-        1000,
+        500,
         1000,
         500,
         1000,
     ]
-    #env = gym.make("LunarLander-v2")
-    #env = gym.make("MountainCar-v0")
+    learners = [
+        DeepQFactory.create_vanilla_deep_q(),
+        DeepQFactory.create_double_deep_q(),
+        DeepQFactory.create_clipped_double_deep_q(),
+        DeepQFactory.create_duel_deep_q(),
+        DeepQFactory.create_double_duel_deep_q(),
+        DeepQFactory.create_clipped_double_duel_deep_q()
+        #ClippedDoubleDeepQ,
+        #DuelDeepQ,
+        #SuperQ,
+    ]
+    # env = gym.make("LunarLander-v2")
+    # env = gym.make("MountainCar-v0")
     env = gym.make("CartPole-v0")
     all_envs = gym.envs.registry.all()
-    #deterministics = [env_spec for env_spec in all_envs if not env_spec.nondeterministic]
+    # deterministics = [env_spec for env_spec in all_envs if not env_spec.nondeterministic]
     ids = [env_spec for env_spec in all_envs if env_spec.id in environments]
-    games = [(env_spec.id, env_spec.max_episode_steps, env_spec.reward_threshold) for env_spec in all_envs if env_spec.id in environments]
-    #games = [env_spec.id, env_spec.rew for env_spec in all]
+    games = [(env_spec.id, env_spec.max_episode_steps, env_spec.reward_threshold) for env_spec in all_envs if
+             env_spec.id in environments]
+    # games = [env_spec.id, env_spec.rew for env_spec in all]
     # sort list to match order in environments
     games = [item for name in environments for item in games if item[0] == name]
     temp = env.reward_range
     data = []
-    #for name, max_episode_steps, reward_threshold in tqdm(games):
+    # for name, max_episode_steps, reward_threshold in tqdm(games):
     for (name, max_episode_steps, reward_threshold), max_episodes in zip(games, games_to_play):
 
-        #bar = tqdm(range(3), bar_format="{postfix[0]} {postfix[1][value]:>8.2g}", postfix=["Learner", dict(value=1]))] desc="Learners")
-        #learner_meter = tqdm(range(3), desc="Learners")
-        #for idx in learner_meter:
-        for idx in range(3):
-            learner_idx = idx % 3
-            if learner_idx == 0:
-                learner = DeepQ
-            elif learner_idx == 1:
-                learner = DoubleDeepQ
-            elif learner_idx == 2:
-                learner = DuelDeepQ
-            #bar.set_postfix(learner.get_name())
-            #print(f"\nPlaying {name} with {learner.get_name()}\n")
+        # bar = tqdm(range(3), bar_format="{postfix[0]} {postfix[1][value]:>8.2g}", postfix=["Learner", dict(value=1]))] desc="Learners")
+        # learner_meter = tqdm(range(3), desc="Learners")
+        # for idx in learner_meter:
+        for learner in learners:
+            # bar.set_postfix(learner.get_name())
+            # print(f"\nPlaying {name} with {learner.get_name()}\n")
 
-            #if name != "LunarLander-v2":
-                #continue
+            # if name != "LunarLander-v2":
+            # continue
             print(f"\nStarting: {name} {learner.get_name()}")
             env = gym.make(name)
             feature_count = env.observation_space.shape[0]
             action_count = env.action_space.n
-            gamma = float(np.power(0.0001, 1./max_episode_steps))  # Scale gamma to approach zero near max_episode_steps
+            gamma = float(
+                np.power(0.0001, 1. / max_episode_steps))  # Scale gamma to approach zero near max_episode_steps
+            learner.build_model(input_dimension=feature_count, output_dimension=action_count,
+                                nodes_per_layer=128,
+                                learning_rate=0.0001,
+                                layer_count=2,
+                                gamma=gamma),
+            start_length = int(max_episodes/10) * max_episode_steps
+            max_possible_step_count = (max_episodes * max_episode_steps) + start_length
+
             agent = Agent(
-                learner=learner(input_dimension=feature_count,
-                                  output_dimension=action_count,
-                                  nodesPerLayer=128,
-                                  learningRate=0.0001,
-                                  numLayers=2,
-                                  gamma=gamma),
-                scorer=Scores(100),
-                sample_size=16,
-                replayBuffer=ReplayBuffer(max_length=1000*max_episode_steps, start_length=10*max_episode_steps),
+                learner=learner,
+                scorer=Scores(10),
+                sample_size=128,
+                #replay_buffer=ReplayBuffer(max_length=1000 * max_episode_steps, start_length=start_length),
+                replay_buffer=ReplayBuffer(max_length=max_possible_step_count, start_length=start_length),
                 environment=env,
-                reward_threshold=reward_threshold,
-                random_choice_decay_min=0.001,
+                reward_threshold=reward_threshold+100,
+                random_choice_decay_min=0.01,
                 max_episode_steps=max_episode_steps,
-                max_episodes=500,
+                max_episodes=max_episodes,
                 verbose=1)
             step_count = agent.play(4000 * max_episode_steps, verbose=1)
             score = agent.score_model(100, verbose=0)
-            #print(f"\n------------ FINAL Average reward: {score} -----------")
+            # print(f"\n------------ FINAL Average reward: {score} -----------")
 
-            #agent.plot(name, learner.get_name())
+            # agent.plot(name, learner.get_name())
             data.append((learner.get_name(), name, step_count, score))
-            #learner_meter.write(f"{learner.get_name()} Done. Final Average Score: {score}")
+            # learner_meter.write(f"{learner.get_name()} Done. Final Average Score: {score}")
             del agent  # Currenlty required to cleanup tqdm status bars when verbose > 0
             print(f"{name} {learner.get_name()} Done. Final Average Score: {score}. Step_count = {step_count}\n")
+            exit(1)
 
     print(data)
 
-    #agent.learner.model.load_weights("model.h5")
+    # agent.learner.model.load_weights("model.h5")
     # pr.enable()
     '''
     start_time = timer()
@@ -125,6 +137,5 @@ if __name__ == "__main__":
 
     # pr.disable()
     agent.learner.model.save_weights("model.h5")
-
 
     # pr.dump_stats('profil.pstat')
