803ea782e06d04a2f3d0a834c95cba7ae7e6ac5a
Ben <benjamin.etheredge@gmail.com>
2019-12-12 21:17:28 -0600

    Added some monitoring for docker cpu usage.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+print(f"cpus: {os.cpu_count()}")
 import tensorflow as tf
 
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
