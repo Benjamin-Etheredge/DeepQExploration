9ac1c77b615f6db359f77aa1893ac2248ba4d63f
Benjamin-Etheredge <benjamin.etheredge@gmail.com>
2020-04-10 15:57:13 +0000

    Adding note for future work.

--- learners/__init__.py
+++ learners/__init__.py
@@ -1,4 +1,4 @@
-from .custom_layers import BellmanLayer
+from .custom_layers import BellmanLayer # why don't I need to import more things?...
 from .custom_layers import QPrimeLayer
 from .deep_q import DeepQ
 from .deep_q_factory import DeepQFactory
